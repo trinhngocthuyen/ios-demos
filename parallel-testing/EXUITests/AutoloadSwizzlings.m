79733504a375ea0879d85f6ba0b27803bfc06ed4
Thuyen Trinh <trinhngocthuyen@gmail.com>
2023-08-20 17:50:07 +0700

    Remove redundant code

--- parallel-testing/EXUITests/AutoloadSwizzlings.m
+++ parallel-testing/EXUITests/AutoloadSwizzlings.m
@@ -5,36 +5,12 @@
 //  Created by Thuyen Trinh on 09/04/2023.
 //
 
-#import <EXUITests-Swift.h>
-
-void DumpObjcMethods(Class clz) {
-
-  unsigned int methodCount = 0;
-  Method *methods = class_copyMethodList(clz, &methodCount);
-
-  printf("Found %d methods on '%s'\n", methodCount, class_getName(clz));
-
-  for (unsigned int i = 0; i < methodCount; i++) {
-    Method method = methods[i];
-
-    printf("\t'%s' has method named '%s' of encoding '%s'\n",
-           class_getName(clz),
-           sel_getName(method_getName(method)),
-           method_getTypeEncoding(method));
-
-    /**
-     *  Or do whatever you need here...
-     */
-  }
-
-  free(methods);
-}
+#import <XCTest/XCTest.h>
+#import <objc/runtime.h>
 
 @implementation XCTestSuite (AutoloadSwizzlings)
 
 + (void)load {
-  [XCTestSuite loadSwizzlings];
-
   Method testClassSuitesForTestIdentifiers = class_getClassMethod(self, NSSelectorFromString(@"testClassSuitesForTestIdentifiers:skippingTestIdentifiers:randomNumberGenerator:"));
   Method hooked_testClassSuitesForTestIdentifiers = class_getClassMethod(self, @selector(hooked_testClassSuitesForTestIdentifiers:skippingTestIdentifiers:randomNumberGenerator:));
   method_exchangeImplementations(testClassSuitesForTestIdentifiers, hooked_testClassSuitesForTestIdentifiers);
@@ -46,16 +22,21 @@ void DumpObjcMethods(Class clz) {
   XCTestSuite *suite = [XCTestSuite hooked_testClassSuitesForTestIdentifiers:arg1 skippingTestIdentifiers:arg2 randomNumberGenerator:arg3];
   for (id testIdentifier in arg1) {
     NSArray<NSString *> *components = [testIdentifier performSelector:NSSelectorFromString(@"components")];
+
+    // Given the input (tokenized test) as "TestCaseA_testA1/testA1",
+    // -> testIdentifier is XCTTestIdentifier(["TestCaseA", "testA1"])
+    // -> components = ["TestCaseA_testA1", "testA1"]
     NSString *testCaseName = [components firstObject];
     NSArray *testCaseComps = [testCaseName componentsSeparatedByString:@"_"];
     NSString *testName = [testCaseComps lastObject];
-    NSLog(@"--> testIdentifier: %@, testCaseName: %@, testName: %@", testIdentifier, testCaseName, testName);
 
+    // Now, if the testCaseName was tokenized, we strip the token
+    // and add the actual suite/test (TestCaseA/testA1) to this suite
     if ([testCaseComps count] > 1 && [[testCaseComps lastObject] isEqual:testName]) {
       NSString *detokenized = [NSString stringWithFormat:@"%@/%@", testCaseComps[0], testName];
-      NSLog(@"--> Detected: detokenized: %@", detokenized);
-      XCTestSuite *proxy = [XCTestSuite testSuiteForTestCaseWithName:detokenized];
-      [suite addTest:proxy];
+      NSLog(@"--> Detokenized: %@", detokenized);
+      XCTestSuite *actual = [XCTestSuite testSuiteForTestCaseWithName:detokenized];
+      [suite addTest:actual];
     }
   }
   return suite;
--- parallel-testing/README.md
+++ parallel-testing/README.md
@@ -2,7 +2,9 @@
 
 Ref: https://engineering.grab.com/tackling-ui-test-execution-time-imbalance-for-xcode-parallel-testing
 
-Disclaimer: The logic was broken in some recent Xcode/iOS releases.
+~~Disclaimer: The logic was broken in some recent Xcode/iOS releases.~~
+
+-> Resolved!
 
 To run the demo
 ```sh
@@ -14,5 +16,5 @@ make demo
 ### Issues
 
 ~~`XCTestSuite.init(name:)` was not picked up by `XCTest` if we passed the unexpected test identifier in the `-only-testing` argument of the `xcodebuild` command.~~
--> Resolved.
 
+-> Resolved!
