05b3f3e0d1494fdaf1a887938b6bd52d1f174439
Thuyen Trinh <trinhngocthuyen@gmail.com>
2023-12-13 20:08:17 +0700

    Update examples

--- cocoapods-spm/LocalPods/Services/Services.podspec
+++ cocoapods-spm/LocalPods/Services/Services.podspec
@@ -1,4 +1,5 @@
 Pod::CompactSpec.new do |s|
   s.name = "Services"
+  s.dependency "Logger"
   s.dependency "Orcam"
 end
--- cocoapods-spm/LocalPods/Services/Sources/Services.swift
+++ cocoapods-spm/LocalPods/Services/Sources/Services.swift
@@ -1,6 +1,9 @@
 import Orcam
+import Logger
 
 @Singleton
 public final class Services {
-
+  func start() {
+    logger.debug("Start services")
+  }
 }
